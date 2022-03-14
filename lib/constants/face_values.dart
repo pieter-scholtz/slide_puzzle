d7a66c38c448be3ddac42abd0cfa4322dbb21763
pieter-scholtz <pieter.scholtz@kdab.com>
2022-03-14 19:15:55 +0200

    polishing

---
+++ lib/constants/face_values.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/rendering.dart';
+
+const Color blue = Color(0xFF0468D7);
+const Color darkBlue = Color(0xFF043875);
+const Color lightBlue = Color(0xFF13B9FD);
+
+const List<Color>  faceColors = [
+  // A,B,C,D,E,F
+  blue, darkBlue, blue, darkBlue, lightBlue, lightBlue
+];
--- lib/models/sizes.dart
+++
@@ -1,5 +0,0 @@
-abstract class TileSize {
-  static double small = 73; //75;
-  static double medium = 103; //100;
-  static double large = 115; //112;
-}
